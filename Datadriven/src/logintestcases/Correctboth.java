7f60473ac6ce1e1ce08dd3ea6f1c495a259a1d95
Ram Senthil <ram.hari993@yahoo.com>
2020-04-19 12:30:01 -0700

    Random Test

---
+++ Datadriven/src/logintestcases/Correctboth.java
@@ -0,0 +1,26 @@
+package src.logintestcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Correctboth {
+    @Test
+    public  void loginwithcorrectUsernameandpwd(){
+
+        System.setProperty("webdriver.chrome.driver", "/Users/ramsubramaniansenthilnathan/Selenium Server/Browser/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com");
+
+        WebElement username = driver.findElement(By.id("txtUsername"));
+        username.sendKeys("Admin");
+
+        WebElement password = driver.findElement(By.id("txtPassword"));
+        password.sendKeys("admin123");
+
+        WebElement loginbtn =  driver.findElement(By.id("btnLogin"));
+        loginbtn.click();
+    }
+}
